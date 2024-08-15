2c7f187dd8dbdc7f340a41b894dd8eb887adc408
D1204480 <161423607+D1204480@users.noreply.github.com>
2024-08-15 17:06:51 +0800

    update

--- shop_app_hw/app/src/main/java/com/example/shop_app_hw/MainActivity.java
+++ shop_app_hw/app/src/main/java/com/example/shop_app_hw/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.shop_app_hw;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    // 開啟首頁
+    public void UniversalIndexPage(View view){
+        Intent i = new Intent();
+        i.setAction(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("https://www.usj.co.jp/web/zh/tw/access"));
+        startActivity(i);
+    }
+
+    public void GoToUniversal(View view){
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("geo:34.664722, 135.433056?q=日本環球影城 ")); // geo是用於地理資訊的字詞
+        startActivity(i);
     }
 }
